e1c0fb9be05bc73d1c89cc05e62f4aa1a7a65c1e
techdiaz1 <92034508+techdiaz1@users.noreply.github.com>
2026-09-25 17:04:14 +0530

    Create groq_llm.py

---
+++ groq_llm.py
@@ -0,0 +1,18 @@
+import os
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
